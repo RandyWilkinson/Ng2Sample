2b633a39378a864cac7db7286f2b0c6dc2375d4b
rwilkinson <randy.wilkinson@outlook.com>
2016-05-19 21:32:26 -0700

    A visual studio 2015 asp.net 5 version of the Angular2 Quickstart app.

---
+++ src/Ng2Sample/gulpfile.js
@@ -0,0 +1,27 @@
+﻿/// <binding AfterBuild='default' Clean='clean' />
+/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var del = require('del');
+
+var paths = {
+    scripts: ['app/**/*.js', 'app/**/*.ts', 'app/**/*.map'],
+    styles: ['styles/**/*'],
+    libs: ['node_modules/**/*.js']
+};
+
+gulp.task('lib', function () {
+    gulp.src(paths.libs).pipe(gulp.dest('wwwroot/scripts/lib'))
+});
+
+gulp.task('clean', function () {
+    return del(['wwwroot/scripts/**/*']);
+});
+
+gulp.task('default', ['lib'], function () {
+    gulp.src(paths.scripts).pipe(gulp.dest('wwwroot/scripts'));
+    gulp.src(paths.styles).pipe(gulp.dest('wwwroot/styles'));
+});
