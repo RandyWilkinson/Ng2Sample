d58abd74bf73424d7f00dda53d1d4f32c5641034
rwilkinson <randy.wilkinson@outlook.com>
2016-05-24 22:26:25 -0700

    Fixing tsconfig file globing and streamlining the gulp tasks, added bootstrap and lodash npm modules

--- src/Ng2Sample/gulpfile.js
+++ src/Ng2Sample/gulpfile.js
@@ -1,16 +1,28 @@
-﻿/// <binding AfterBuild='default' Clean='clean' />
+﻿/// <binding AfterBuild='default' Clean='clean' BeforeBuild='tsconfig' />
 /*
 This file is the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 
-var gulp = require('gulp');
-var del = require('del');
+var gulp = require('gulp'),
+del = require('del'),
+webserver = require('gulp-webserver'),
+watch = require('gulp-watch'),
+gulpTsConfig = require('gulp-tsconfig');
 
 var paths = {
-    scripts: ['app/**/*.js', 'app/**/*.ts', 'app/**/*.map'],
-    styles: ['styles/**/*'],
-    libs: ['node_modules/**/*.js']
+    app: ['app/**/*.js', 'app/**/*.ts', 'app/**/*.map', 'app/**/*.css', 'app/**/*.html'],
+    styles: ['styles/**/*', 'node_modules/bootstrap/dist/css/bootstrap.min.css'],
+    libs: ['node_modules/@angular*/**/*.js',
+    'node_modules/rxjs*/**/*.js',
+    'node_modules/angular2-in-memory-web-api*/**/*.js',
+    'node_modules/es6-shim*/**/*.js',
+    'node_modules/zone.js*/**/*.js',
+    'node_modules/reflect-metadata*/**/*.js',
+    'node_modules/systemjs*/**/*.js',
+    'node_modules/lodash/dist/lodash.min.js',
+    'node_modules/bootstrap/dist/js/bootstrap.min.js'
+    ]
 };
 
 gulp.task('lib', function () {
@@ -18,10 +30,52 @@ gulp.task('lib', function () {
 });
 
 gulp.task('clean', function () {
-    return del(['wwwroot/scripts/**/*']);
+    return del(['wwwroot/scripts/**/*', 'wwwroot/styles/**/*']);
 });
 
 gulp.task('default', ['lib'], function () {
-    gulp.src(paths.scripts).pipe(gulp.dest('wwwroot/scripts'));
+    gulp.src(paths.app).pipe(gulp.dest('wwwroot/scripts'));
     gulp.src(paths.styles).pipe(gulp.dest('wwwroot/styles'));
+});
+
+gulp.task('stream', function () {
+    return gulp.src(paths.app)
+        .pipe(watch(paths.app))
+        .pipe(gulp.dest('wwwroot/scripts'));
+});
+
+gulp.task('webserver', function () {
+    gulp.src('wwwroot')
+      .pipe(webserver({
+          livereload: true,
+          fallback: 'index.html',
+          open: true
+      }));
+});
+
+gulp.task('tsconfig', function () {
+    var tsConfig = gulpTsConfig({
+        tsOrder: [
+            '**/app.module.ts',
+            '**/*.module.ts',
+            '**/*.ts'],
+        tsConfig: {
+            "compilerOptions": {
+                "noImplicitAny": false,
+                "noEmitOnError": true,
+                "sourceMap": true,
+                "experimentalDecorators": true,
+                "emitDecoratorMetadata": true,
+                "target": "es5"
+            },
+            "compileOnSave": true
+
+        }
+    });
+
+    return gulp.src(["app/**/*.ts", "typings/index.d.ts"])
+        .pipe(tsConfig())
+        .pipe(gulp.dest('.'));
+
+    // --> result is a tsconfig.json file. 
 });
