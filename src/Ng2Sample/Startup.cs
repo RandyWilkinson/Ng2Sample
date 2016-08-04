091ce068646bdf168298fa5716d325bab595d362
rwilkinson <randy.wilkinson@outlook.com>
2016-08-04 11:11:42 -0700

    Bringing the project over to asp.net core 1.0

--- src/Ng2Sample/Startup.cs
+++ src/Ng2Sample/Startup.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Ng2Sample
 {
@@ -18,14 +19,17 @@ namespace Ng2Sample
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            app.UseIISPlatformHandler();
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-        }
+            loggerFactory.AddConsole();
 
-        // Entry point for the application.
-        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            app.UseFileServer();
+            
+        }
     }
 }
